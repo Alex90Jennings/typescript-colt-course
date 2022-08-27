3b872472c8fa87b785d306ce427d09d6c94aab87
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-08-27 18:47:45 +0200

    generics

--- dist/generics.js
+++ dist/generics.js
@@ -1,5 +1,6 @@
 "use strict";
-function identity(item) {
-    return item;
+function getRandomElement(list) {
+    const randomIndex = Math.floor(Math.random() * list.length);
+    return list[randomIndex];
 }
-console.log(typeof identity(["klasfjd", "kldasjflk"]));
+console.log(getRandomElement([1, 2, 3, 4, 5, 6]));
--- src/generics.ts
+++ src/generics.ts
@@ -1,5 +1,7 @@
-function identity<T>(item: T): T{
-    return item
+
+function getRandomElement<T>(list: T[]): T {
+    const randomIndex = Math.floor(Math.random() * list.length)
+    return list[randomIndex]
 }
 
-console.log(typeof identity(["klasfjd", "kldasjflk"]))
+console.log(getRandomElement([1, 2, 3, 4, 5, 6]))
