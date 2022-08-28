f43be029ee5427d45cd5ce19a93f040aa5354baf
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-08-28 17:23:17 +0200

    modules

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_js_1 = require("./utils.js");
-console.log((0, utils_js_1.findRandom)([1, 2, 3, 4, 5, 6]));
-console.log((0, utils_js_1.add)(10, 20));
+import { add, findRandom } from "./utils.js";
+console.log(findRandom([1, 2, 3, 4, 5, 6]));
+console.log(add(10, 20));
--- dist/utils.js
+++ dist/utils.js
@@ -1,12 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = exports.findRandom = void 0;
-function findRandom(arr) {
+export function findRandom(arr) {
     const index = Math.floor(Math.random() * arr.length);
     return arr[index];
 }
-exports.findRandom = findRandom;
-function add(x, y) {
+export function add(x, y) {
     return x + y;
 }
-exports.add = add;
